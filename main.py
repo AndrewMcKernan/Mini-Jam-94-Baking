a33cb52d68cb6936673aa967a8e3d085d1b843c0
Andrew McKernan <amckerna@ualberta.ca>
2021-11-27 15:32:14 -0700

    Added display area on bottom that displays unit info and controls

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, sys
 import pygame
 from constants import *
 from draw_text import drawText
-from sprites import AlliedUnit, HostileUnit, UnitType
+from sprites import AlliedUnit, HostileUnit, UnitType, MouseSprite
 
 if getattr(sys, 'frozen', False):
     os.chdir(sys._MEIPASS)
@@ -24,6 +24,9 @@ ZOOMED_BACKGROUND = pygame.Rect(0, 0, ZOOMED_MAZE_WIDTH, ZOOMED_MAZE_HEIGHT)
 EGG_IMAGE = pygame.image.load(os.path.join('assets', 'egg.png')).convert()
 EGG_IMAGE.set_colorkey(TRANSPARENT)
 
+MOUSE_IMAGE = pygame.image.load(os.path.join('assets', 'mouse.png')).convert()
+MOUSE_IMAGE.set_colorkey(TRANSPARENT)
+
 MAP_IMAGES = dict()
 
 #pygame.mixer.music.load(os.path.join('assets', 'Spy.mp3'))
@@ -31,6 +34,7 @@ MAP_IMAGES = dict()
 #pygame.mixer.music.play(-1)
 
 EGG_SURFACE = pygame.transform.scale(EGG_IMAGE, (TILE_WIDTH, TILE_HEIGHT))
+MOUSE_SURFACE = pygame.transform.scale(MOUSE_IMAGE, (200, 200))
 #ZOOMED_BUDDY = pygame.transform.scale(BUDDY_IMAGE, (ZOOMED_BUDDY_WIDTH, ZOOMED_BUDDY_HEIGHT))
 
 TEXT_FONT = pygame.font.SysFont('lucidaconsole', 40)
@@ -59,8 +63,13 @@ def mouse_pos_to_grid_coords(mouse_pos):
     return mouse_pos[0] // TILE_WIDTH, mouse_pos[1] // TILE_HEIGHT
 
 
-def draw_window(cursor_xy, fps, allied_units):
-    pygame.draw.rect(WIN, BLACK, pygame.Rect(0, 0, WIDTH, HEIGHT))
+def draw_window(cursor_xy, fps, allied_units, sprite_to_display):
+    pygame.draw.rect(WIN, GRAY, pygame.Rect(0, 0, WIDTH, HEIGHT))
+
+    bottom_left_rect = pygame.Rect(0, HEIGHT - 200, 200, 200)
+    middle_rect = pygame.Rect(200, HEIGHT - 150, WIDTH - 400, 150)
+    bottom_right_rect = pygame.Rect(WIDTH - 200, HEIGHT - 200, 200, 200)
+
 
     x = 0
     while x < WIDTH:
@@ -77,11 +86,36 @@ def draw_window(cursor_xy, fps, allied_units):
 
     allied_units.draw(WIN)
 
+    pygame.draw.rect(WIN, BLACK, bottom_left_rect)
+    WIN.blit(MOUSE_SURFACE, (bottom_left_rect.x, bottom_left_rect.y))
+    pygame.draw.rect(WIN, BLACK, bottom_right_rect)
+    pygame.draw.rect(WIN, BLACK, middle_rect)
+    if sprite_to_display is None:
+        pass
+    else:
+        drawText(WIN, sprite_to_display.unitType.name, WHITE, middle_rect, TEXT_FONT, True)
+        next_rect = pygame.Rect(middle_rect.x, middle_rect.y + TEXT_FONT.get_height() + 3, middle_rect.width, middle_rect.height)
+        drawText(WIN, "HP: " + str(sprite_to_display.hp) + "/" + str(sprite_to_display.max_hp), WHITE, next_rect, TEXT_FONT, True)
+        next_rect = pygame.Rect(next_rect.x, next_rect.y + TEXT_FONT.get_height() + 3, next_rect.width,
+                                next_rect.height)
+        drawText(WIN, "Movement: " + str(sprite_to_display.movement), WHITE, next_rect, TEXT_FONT, True)
+
     pygame.display.update()
+
+
+all_sprites = pygame.sprite.Group()
+
+
+def add_sprite_to_group(sprite, group):
+    group.add(sprite)
+    # do not add the sprite twice if the group given is all_sprites
+    if not group == all_sprites:
+        all_sprites.add(sprite)
 
 
 def game():
     restart = False
+    all_sprites.empty()
     clock = pygame.time.Clock()
     title_mode = True
     run = True
@@ -99,8 +133,10 @@ def game():
     starting_xy = coordinates_to_xy((GRID_WIDTH // 2, GRID_HEIGHT // 2))
 
     egg_soldier = AlliedUnit(EGG_IMAGE, TILE_WIDTH, TILE_HEIGHT, UnitType.EGG, starting_xy[0], starting_xy[1])
+
     allied_units = pygame.sprite.Group()
-    allied_units.add(egg_soldier)
+    add_sprite_to_group(egg_soldier, allied_units)
+    sprite_to_display = None
 
     while run:
         clock.tick(FPS)
@@ -110,6 +146,8 @@ def game():
                 run = False
                 pygame.quit()
                 break
+            if event.type == pygame.MOUSEMOTION:
+                pass
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # these are the values of event.button depending on which is pressed:
                 # 1 - left click
@@ -121,9 +159,20 @@ def game():
                 if event.button == 1:
                     # left click
                     cursor_grid_coordinates = mouse_pos_to_grid_coords(pygame.mouse.get_pos())
+                    mouse_sprite = MouseSprite(pygame.mouse.get_pos())
+                    mouse_colliding = pygame.sprite.spritecollide(mouse_sprite, all_sprites, False)
+                    if len(mouse_colliding) > 1:
+                        # lol what
+                        pass
+                    elif len(mouse_colliding) == 1:
+                        # display the unit information on the bottom screen
+                        sprite_to_display = mouse_colliding[0]
+                    else:
+                        # no unit is being hovered
+                        sprite_to_display = None
                 if event.button == 3:
                     # right click
-                    pass
+                    sprite_to_display = None
                 # as per the jam restrictions, these are the only allowed buttons
 
             # escape should be allowed, right?
@@ -134,7 +183,7 @@ def game():
                     continue
         if not run:
             continue
-        draw_window(coordinates_to_xy(cursor_grid_coordinates), frames_string, allied_units)
+        draw_window(coordinates_to_xy(cursor_grid_coordinates), frames_string, allied_units, sprite_to_display)
 
         frames += 1
         current_time_ms = pygame.time.get_ticks() % 1000
--- sprites.py
+++ sprites.py
@@ -6,6 +6,13 @@ class UnitType(Enum):
     EGG = 1
 
 
+class MouseSprite(pygame.sprite.Sprite):
+    def __init__(self, position_tuple):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = None
+        self.rect = pygame.rect.Rect(position_tuple, (1, 1))
+
 class Unit(pygame.sprite.Sprite):
     def __init__(self, image, width, height, unit_type, start_x=0, start_y=0):
         pygame.sprite.Sprite.__init__(self)
@@ -17,6 +24,16 @@ class Unit(pygame.sprite.Sprite):
         if not isinstance(unit_type, UnitType):
             raise TypeError("Unit Type must be set to a UnitType enum.")
         self.unitType = unit_type
+
+        if unit_type == UnitType.EGG:
+            self.hp = 5
+            self.max_hp = 5
+            self.movement = 5
+        else:
+            # this should never happen
+            self.hp = 1
+            self.max_hp = 1
+            self.movement = 0
 
 
 # not sure if these two classes are really needed.
